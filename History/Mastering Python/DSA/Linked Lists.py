e187441067a7c47a452213c23f58748cc6698f78
Aditya Singh <44133844+NyanAditya@users.noreply.github.com>
2024-05-14 11:32:29 +0530

    chore: Add get_position, insert, and delete functions to LinkedList class

--- History/Mastering Python/DSA/Linked Lists.py
+++ History/Mastering Python/DSA/Linked Lists.py
@@ -1,24 +1,12 @@
-"""The LinkedList code from before is provided below.
-Add three functions to the LinkedList.
-"get_position" returns the element at a certain position.
-The "insert" function will add an element to a particular
-spot in the list.
-"delete" will delete the first element with that
-particular value.
-Then, use "Test Run" and "Submit" to run the test cases
-at the bottom."""
-
-
 class Element(object):
     def __init__(self, value):
         self.value = value
         self.next = None
-
-
+        
 class LinkedList(object):
     def __init__(self, head=None):
         self.head = head
-
+        
     def append(self, new_element):
         current = self.head
         if self.head:
@@ -27,24 +15,45 @@ class LinkedList(object):
             current.next = new_element
         else:
             self.head = new_element
-
+            
     def get_position(self, position):
         """Get an element from a particular position.
         Assume the first position is "1".
         Return "None" if position is not in the list."""
-        return None
-
+        
+        current = self.head
+        
+        if self.head:
+            current_pos = 1
+            
+            while current:
+                current = current.next
+                current_pos += 1
+            
+            if current_pos == position:
+                return current
+                
+            return None
+            
+        else:
+            return None
+    
     def insert(self, new_element, position):
         """Insert a new node at the given position.
         Assume the first position is "1".
         Inserting at position 3 means between
         the 2nd and 3rd elements."""
-        pass
-
+        
+        current = self.head
+        while current.next != position:
+            current = current.next
+            
+        
+    
+    
     def delete(self, value):
         """Delete the first node with a given value."""
         pass
-
 
 # Test cases
 # Set up some Elements
@@ -60,20 +69,21 @@ ll.append(e3)
 
 # Test get_position
 # Should print 3
-print ll.head.next.next.value
+print (ll.head.next.next.value)
 # Should also print 3
-print ll.get_position(3).value
+print (ll.get_position(3).value)
 
 # Test insert
-ll.insert(e4, 3)
+ll.insert(e4,3)
 # Should print 4 now
-print ll.get_position(3).value
+print (ll.get_position(3).value)
 
 # Test delete
 ll.delete(1)
+
 # Should print 2 now
-print ll.get_position(1).value
+print (ll.get_position(1).value)
 # Should print 4 now
-print ll.get_position(2).value
+print (ll.get_position(2).value)
 # Should print 3 now
-print ll.get_position(3).value
+print (ll.get_position(3).value)
