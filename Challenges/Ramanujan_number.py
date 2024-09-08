6cfea1ed580c0283aad6dbb63367be489429ef2a
Aditya Singh <44133844+NyanAditya@users.noreply.github.com>
2024-09-08 21:54:21 +0530

    Add Ramanujan number challenge

---
+++ Challenges/Ramanujan_number.py
@@ -0,0 +1,9 @@
+"""
+Calculate the sum of two numbers using the Ramanujan method.
+
+A Ramanujan number is a number that can be expressed as the sum of two cubes in two different ways.
+
+Example:
+Input: 1729
+Output: The sum of two cubes that equals 1729 can be expressed as 1^3 + 12^3 or 9^3 + 10^3.
+"""
