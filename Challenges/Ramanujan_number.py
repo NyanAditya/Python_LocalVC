e52868b96b7ed28825160c798006787746eb32a6
Aditya Singh <44133844+NyanAditya@users.noreply.github.com>
2024-09-16 04:03:47 +0530

    feat: Implement Ramanujan number calculation and add project configuration files

--- Challenges/Ramanujan_number.py
+++ Challenges/Ramanujan_number.py
@@ -1,9 +1,36 @@
 """
-Calculate the sum of two numbers using the Ramanujan method.
-
 A Ramanujan number is a number that can be expressed as the sum of two cubes in two different ways.
 
 Example:
 Input: 1729
 Output: The sum of two cubes that equals 1729 can be expressed as 1^3 + 12^3 or 9^3 + 10^3.
 """
+
+
+def isRamanujan(num: int) -> str:
+    
+    Ramanujan_cube_pairs: list[(int, int)] = []
+    
+    i = j = 1
+    
+    while i**3 <= num:
+        
+        j = 1
+    
+        while (i**3 + j**3) <= num:
+            
+            if (i**3 + j**3) == num:
+                Ramanujan_cube_pairs.append((j, i))
+            
+            if len(Ramanujan_cube_pairs) == 2:
+                return f"Yes, {num} = {Ramanujan_cube_pairs[0][0]}³ + {Ramanujan_cube_pairs[0][1]}³ = {Ramanujan_cube_pairs[1][0]}³ + {Ramanujan_cube_pairs[1][1]}³"
+            
+            j += 1
+            
+        i += 1
+            
+    return "No!"
+        
+        
+num: int = int(input("Enter a Number: "))
+print(isRamanujan(num))
